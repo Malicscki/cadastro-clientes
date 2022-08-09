4ed61ce439273f4ceeb510d565fe40793f7ebdb7
Malicscki <106198134+Malicscki@users.noreply.github.com>
2022-08-09 19:47:17 -0300

    atualizando js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict'     //https://www.youtube.com/watch?v=_HEIqE_qqbQ
 
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
@@ -7,8 +7,8 @@ const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
 const tempClient = {
-    nome: "Joao",
-    email: "luiz.malicscki@gmail.com",
+    nome: "Nicolas",
+    email: "nnicolas@gmail.com",
     celular: "47984759767",
     cidade: "Blumenau SC"
 }
@@ -27,6 +27,47 @@ const createClient = (client) => {
 //CRUD - READ
 const readClient = () => getLocalStorage()
 
+//CRUD - UPDATE
+const updateClient = (index, client) => {
+    const dbClient = readClient()
+    dbCLient[index] = client
+    setLocalStorage(dbClient)
+}
+
+//CRUD - DELETE
+const deleteClient = (index) => {
+    const dbClient = readClient()
+    dbClient.splice(index,1)
+    setLocalStorage(dbClient)
+}
+
+const isValidFields = () => {
+    return document.getElementById('form').reportValidity()
+
+}
+
+const clearFields = () =>{
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "") //forEach faz uma varredura em todos campos do form e a continuaçao da const é pra deixar em branco
+}
+
+// INTERAÇAO COM O LAYOUT
+const saveClient = () => {
+    if (isValidFields()) {
+        const client ={
+            nome: document.getElementById('nome').value,
+            email: document.getElementById('email').value,
+            celular: document.getElementById('celular').value,
+            cidade: document.getElementById('cidade').value
+        }
+        createClient(client)
+        clearFields()
+        closeModal()
+    }
+
+}
+
+
 //Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
@@ -34,5 +75,14 @@ document.getElementById('cadastrarCliente')
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
 
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
 
-    //https://www.youtube.com/watch?v=_HEIqE_qqbQ
+
+
+
+
+
+
+
+
